78f2c6aa3c0b41db5aadbb42685f2fac220372eb
Oscar Bernardini <bernardini687@gmail.com>
2019-10-19 20:48:48 +0200

    test the cli

---
+++ test/cli_test.exs
@@ -0,0 +1,18 @@
+defmodule CliTest do
+  use ExUnit.Case
+
+  import Issues.CLI, only: [parse_argv: 1]
+
+  test "returns :help if given -h or --help options" do
+    assert parse_argv(["-h", "foo"]) == :help
+    assert parse_argv(["--help", "bar"]) == :help
+  end
+
+  test "returns three values if given three options" do
+    assert parse_argv([?a, ?b, "4"]) == {?a, ?b, 4}
+  end
+
+  test "defaults count if given two options" do
+    assert parse_argv([?a, ?b]) == {?a, ?b, 4}
+  end
+end
