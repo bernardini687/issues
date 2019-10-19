eaf5f034f2e6bda02fa48593f6f21bcdc8ca9f89
Oscar Bernardini <bernardini687@gmail.com>
2019-10-19 19:07:07 +0200

    add parse functionality

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,3 +1,22 @@
 defmodule Issues.CLI do
+  @default_count 4
 
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(
+      argv,
+      switches: [help: :boolean],
+      alias: [h: :help]
+    )
+
+    case parse do
+      {[help: true], _, _} -> :help
+      {_, [user, project, count], _} -> {user, project, count}
+      {_, [user, project], _} -> {user, project, @default_count}
+      _ -> :help
+    end
+  end
 end
