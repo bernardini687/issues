f586b12969bda1dad3601021aaf92cb7fca2e918
Oscar Bernardini <bernardini687@gmail.com>
2019-10-24 14:30:44 +0200

    add logger

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -3,10 +3,14 @@ defmodule Issues.GitHubIssues do
   Handles the request to the GitHub API.
   """
 
+  require Logger
+
   @github_url Application.get_env(:issues, :github_url)
   @user_agent [{"User-Agent", "bernardini687/issues"}]
 
   def fetch(user, project) do
+    Logger.info "fetching user #{user}'s project #{project}"
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
@@ -17,6 +21,9 @@ defmodule Issues.GitHubIssues do
   end
 
   def handle_response({:ok, %{body: body}}) do
+    Logger.info "successful response"
+    Logger.debug fn -> inspect(body) end
+
     case Jason.decode(body, keys: :atoms) do
       {:ok, body} ->
         {:ok, body}
@@ -27,6 +34,7 @@ defmodule Issues.GitHubIssues do
   end
 
   def handle_response({:error, %{reason: details}}) do
+    Logger.error "error #{details} returned"
     {:error, details}
   end
 end
